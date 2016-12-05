42131e5fb9c49d5e2e4e6389cf5c3335f462abef
philippspo <philipp.sporrer@commercetools.de>
2016-12-05 21:58:57 +0100

    chore(linter): add eslint

--- js/app.js
+++ js/app.js
@@ -4,54 +4,52 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
     AppRegistry,
     StyleSheet,
-    Text,
     View,
-    StatusBar
+    StatusBar,
 } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 import Map from './meetups/map';
 import List from './meetups/list';
+import chevron from '../assets/back_chevron.png';
 
-export class App extends Component {    
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  navBar: {
+    backgroundColor: '#5555FF',
+  },
+  navBarText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
-    render() {
-        return (
-            <View style={{flex:1}}>
-                <StatusBar barStyle="light-content"/>
-                <Router>
-                    <Scene key="root">
-                        <Scene navigationBarStyle={styles.navBar}
-                               backButtonImage={require('./../assets/back_chevron.png')}
-                               titleStyle={styles.navBarText} key="map" component={Map} title="Meetup Location"/>
-                        <Scene navigationBarStyle={styles.navBar}
-                               titleStyle={styles.navBarText} key="list" component={List} title="Meetups" initial={true}/>
-                    </Scene>
-                </Router>
-            </View>
-        );
-    }
-}
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <StatusBar barStyle="light-content" />
+    <Router>
+      <Scene key="root">
+        <Scene
+          navigationBarStyle={styles.navBar}
+          backButtonImage={chevron}
+          titleStyle={styles.navBarText} key="map" component={Map} title="Meetup Location"
+        />
+        <Scene
+          navigationBarStyle={styles.navBar}
+          titleStyle={styles.navBarText} key="list" component={List} title="Meetups" initial
+        />
+      </Scene>
+    </Router>
+  </View>
+);
 
-let styles = {
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        justifyContent:  'flex-end',
-        flex:            1,
-        alignItems:      'center',
-        backgroundColor: '#F5FCFF',
-    },
-    navBar: {
-        backgroundColor: '#5555FF',
-    },
-    navBarText: {
-        color: 'white',
-        fontWeight: 'bold',
-    }
-};
-
-styles = StyleSheet.create(styles);
 AppRegistry.registerComponent('ReactNatives', () => App);
--- js/meetups/list.js
+++ js/meetups/list.js
@@ -6,102 +6,19 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
   View,
   ListView,
   TouchableHighlight,
-  Linking,
-  Alert
 } from 'react-native';
+import moment from 'moment';
 import { Actions } from 'react-native-router-flux';
 import events from '../../database/events.json';
-import moment from 'moment';
-
-export default class List extends Component {
-
-  constructor(props) {
-    console.log(events);
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(events),
-    };
-    this._renderRow = this._renderRow.bind(this);
-    this._pressRow = this._pressRow.bind(this);
-  }
-
-  _pressRow(rowID: number) {
-    Actions.map();
-  }
-
-  _renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
-    let rowHash = Math.abs(hashCode(rowData));
-    return (
-      <TouchableHighlight onPress={() => {
-          this._pressRow(rowID);
-          //highlightRow(sectionID, rowID);
-      }}>
-        <View style={{flexDirection: 'row', backgroundColor: 'white'}}>
-          <View style={styles.row}>
-            <Text style={styles.text}>
-              {moment(rowData.time).format('LL')}, {rowData.location.city}
-            </Text>
-            <Text style={styles.title}>
-                {rowData.group} 
-                
-            </Text>
-         
-<Text style={styles.description}>
-                {rowData.title}
-            </Text>
-
-          </View>
-        </View>
-      </TouchableHighlight>
-    );
-  }
-
-  _renderSeparator (sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
-  return (
-    <View key={`${sectionID}-${rowID}`}
-          style={{
-            height: adjacentRowHighlighted ? 4 : 1,
-            backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
-            marginLeft: 15
-          }}
-    />
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ListView
-          style={{flex:1}}
-          dataSource={this.state.dataSource}
-          renderRow={this._renderRow}
-          renderSeparator={this._renderSeparator}
-        />
-      </View>
-    );
-  }
-}
-
-/* eslint no-bitwise: 0 */
-let hashCode = function(str) {
-  let  hash = 15;
-  for (let ii = str.length - 1; ii >= 0; ii--) {
-    hash = ((hash << 5) - hash) + str.charCodeAt(ii);
-  }
-  return hash;
-};
-
 
 let styles = {
   container: {
-    //...StyleSheet.absoluteFillObject,
+    // ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
     flex: 1,
     backgroundColor: 'white',
@@ -113,7 +30,7 @@ let styles = {
     textAlign: 'left',
     margin: 3,
     color: 'black',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
   },
   text: {
     fontSize: 12,
@@ -121,7 +38,7 @@ let styles = {
     textAlign: 'left',
     margin: 3,
     color: 'gray',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
@@ -134,11 +51,85 @@ let styles = {
   },
 };
 
-styles.description =  {
+styles.description = {
   ...styles.text,
   fontSize: 14,
-  color: 'black'
+  color: 'black',
 };
 
 styles = StyleSheet.create(styles);
 
+export default class List extends Component {
+
+  static pressRow() {
+    Actions.map();
+  }
+
+  static renderSeparator(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+    return (
+      <View
+        key={`${sectionID}-${rowID}`}
+        style={{
+          height: adjacentRowHighlighted ? 4 : 1,
+          backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+          marginLeft: 15,
+        }}
+      />
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows(events),
+    };
+    this.renderRow = this.renderRow.bind(this);
+    this.pressRow = this.pressRow.bind(this);
+  }
+
+  renderRow(
+    rowData: string,
+    sectionID: number,
+    rowID: number,
+  ) {
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          this.pressRow(rowID);
+          // highlightRow(sectionID, rowID);
+        }}
+      >
+        <View style={{ flexDirection: 'row', backgroundColor: 'white' }}>
+          <View style={styles.row}>
+            <Text style={styles.text}>
+              {moment(rowData.time).format('LL')}, {rowData.location.city}
+            </Text>
+            <Text style={styles.title}>
+              {rowData.group}
+
+            </Text>
+
+            <Text style={styles.description}>
+              {rowData.title}
+            </Text>
+
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          style={{ flex: 1 }}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+          renderSeparator={this.renderSeparator}
+        />
+      </View>
+    );
+  }
+}
