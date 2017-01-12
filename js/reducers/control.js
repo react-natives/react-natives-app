7f69f5658a2e45b5d7207294b883103d7187a150
Thomas Kolar <thomas.kolar@email.de>
2017-01-12 17:30:34 +0100

    Adds pure function action handlers

--- js/reducers/control.js
+++ js/reducers/control.js
@@ -5,5 +5,18 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-    return state
+    alert(JSON.stringify(action));
+    let nextState = state;
+    switch(action.type) {
+        case 'TOGGLE_VISIBILITY':
+            if (nextState.visibleGroups.indexOf(action.group) >= 0) {
+                nextState.visibleGroups.splice(nextState.visibleGroups.indexOf(action.group), 1);
+            }
+            else {
+                nextState.visibleGroups.push(action.group);
+            }
+            return nextState;
+        default:
+            return state
+    }
 }
--- js/reducers/events.js
+++ js/reducers/events.js
@@ -3,5 +3,14 @@ import events from '../../database/events.json'
 const initialState = events
 
 export default (state = initialState, action) => {
-    return state
+    alert(JSON.stringify(action));
+    let nextState = state;
+    switch (action.type) {
+        case 'DELETE_EVENT':
+            return nextState.events(action.index, 1);
+        case 'ADD_EVENT':
+            return nextState.push(action.event);
+        default:
+            return state;
+    }
 }
