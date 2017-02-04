0ddd8db6cbc2748fbcf4fdadc26c7719b1e21578
Thomas Kolar <thomas.kolar@email.de>
2017-02-04 20:27:54 +0100

    Introduces the constants directory

---
+++ js/constants/action-types.js
@@ -0,0 +1,5 @@
+export const ADD_SLIDE = 'ADD_SLIDE'
+export const EDIT_SLIDE = 'EDIT_SLIDE'
+export const DELETE_SLIDE = 'DELETE_SLIDE'
+export const EDIT_SETTINGS = 'EDIT_SETTINGS'
+export const UPDATE_SYNC_STATE = 'UPDATE_SYNC_STATE'
